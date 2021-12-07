a13924536bcbe577dcb4eec039e1b46d9269b17e
Jamie <lijamie19828@gmail.com>
2021-12-07 15:08:54 -0800

    create tests for DayListItem, edit DayListItem to change h3 based on number of spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,10 +11,21 @@ export default function DayListItem(props) {
   "day-list__item--full": props.spots === 0
   });
 
+  const spotCheck = (spots) => {
+    if (spots === 0) {
+      return "no spots"
+    }
+    return (spots === 1) ? "1 spot " : `${spots} spots `
+  }
+
   return (
     <li onClick={() => props.setDay(props.name)} className={listClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spotCheck(props.spots)} remaining</h3>
     </li>
   );
 }
+
+// {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+// {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
+// {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
