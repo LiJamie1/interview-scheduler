924dc3d175cd0ba27972e8df0176404f8dc2ac7e
Jamie <lijamie19828@gmail.com>
2021-12-08 17:28:47 -0800

    delete excess files for clarity, write Header component

---
+++ src/components/Appointment/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(props) {
+  return (
+    <header className="appointment__time">
+      <h4 className="text--semi-bold">12pm</h4>
+      <hr className="appointment__separator" />
+    </header>
+
+  )
+}
