d2bd5593a9a08c72f9ba294fc1e3b3ec4dad7cee
Jamie <lijamie19828@gmail.com>
2021-12-08 17:59:27 -0800

    Write Show and related story. Create Confirm.js

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 
 storiesOf("Button", module)
@@ -145,3 +146,4 @@ storiesOf("InterviewerList", module)
     .add("Appointment with time", () => (<Appointment time="12pm" />))
     .add("Header", () => (<Header time="12pm" />))
     .add("Empty", () => (<Empty onAdd={action("onAdd")} />))
+    .add("Show", () => (<Show student="Jamie Li" interviewer={interviewers[0]} onEdit={action("onEdit")} onDelete={action("onDelete")} />))
