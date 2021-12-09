bb30dd586253352e0fb26a046a4a6103cb44798e
Jamie <lijamie19828@gmail.com>
2021-12-08 19:16:13 -0800

    Write Form with cancel button, reset function and input complete. Stories written for Form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  // States
+  // student: String and interviewer:  Number
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function() {
+    setStudent("")
+    setInterviewer(null)
+  }
+
+  // props
+  // student: String
+  // interviewers: Array
+  // interviewer: number
+  // onSave: function
+  // onCancel: function
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name={props.student}
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={() => {
+            reset()
+            props.onCancel()
+          }}>Cancel</Button>
+        <Button confirm /* your code goes here */>Save</Button>
+      </section>
+    </section>
+    </main >
+
+  )
+}
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -154,3 +155,5 @@ storiesOf("InterviewerList", module)
     .add("Confirm", () => (<Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />))
     .add("Status", () => (<Status message="Deleting" />))
     .add("Error", () => (<Error message="Could not delete appointment." onClose={action("onClose")} />))
+    .add("Edit Form", () => (<Form student="Jamie Li" interviewer={3} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />))
+    .add("Create Form", () => (<Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />))
