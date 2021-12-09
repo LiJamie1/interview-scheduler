7c2b8ba5a4d94c3734f60d4f965867d0630fa01a
Jamie <lijamie19828@gmail.com>
2021-12-08 18:23:39 -0800

    Write Status component and story

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm"
+import Status from "components/Appointment/Status"
 
 
 storiesOf("Button", module)
@@ -149,3 +150,4 @@ storiesOf("InterviewerList", module)
     .add("Empty", () => (<Empty onAdd={action("onAdd")} />))
     .add("Show", () => (<Show student="Jamie Li" interviewer={interviewers[0]} onEdit={action("onEdit")} onDelete={action("onDelete")} />))
     .add("Confirm", () => (<Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />))
+    .add("Status", () => (<Status message="Deleting" />))
