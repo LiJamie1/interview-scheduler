a1621de5e3d8e754bd4f11ed65177c61ff2dedab
Jamie <lijamie19828@gmail.com>
2021-12-08 17:21:48 -0800

    add stories and update index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import "components/Appointment/styles.scss"
 
 export default function Appointment(props) {
+
+  const appointmentText = function (props) {
+    if (props.time) {
+      return `Appointment at ${props.time}`
+    }
+    return "No Appointments"
+  }
+
   return (
-    <article className="appointment"></article>
+    <article className="appointment">{appointmentText(props)}</article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,8 @@ import DayListItem from "components/DayListItem"
 import DayList from "components/DayList"
 import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
+import Appointment from "components/Appointment/index.js";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -133,3 +135,13 @@ storiesOf("InterviewerList", module)
     />
   ));
 
+  storiesOf("Appointment", module)
+    .addParameters({
+      backgrounds: [{ name: "white", value:"#fff", default: true}]
+    })
+    .add("Appointment", () => (<Appointment/>))
+    .add("Appointment with time", () => (
+      <Appointment 
+        time="12pm"
+      />
+    ))
