8602238954bf96671d677aaa61250359e4eaded8
Jamie <lijamie19828@gmail.com>
2021-12-08 18:17:19 -0800

    Write Confirm component and stories

---
+++ src/components/Appointment/Confirm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "components/Button";
+
+export default function Confirm(props) {
+  return (
+    <main className="appointment__card appointment__card--confirm">
+      <h1 className="text--semi-bold">{props.message}</h1>
+      <section className="appointment__actions">
+        <Button onClick={props.onCancel}danger>Cancel</Button>
+        <Button onClick={props.onConfirm}danger>Confirm</Button>
+      </section>
+    </main>
+
+  )
+}
--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm"
 
 
 storiesOf("Button", module)
@@ -147,3 +148,4 @@ storiesOf("InterviewerList", module)
     .add("Header", () => (<Header time="12pm" />))
     .add("Empty", () => (<Empty onAdd={action("onAdd")} />))
     .add("Show", () => (<Show student="Jamie Li" interviewer={interviewers[0]} onEdit={action("onEdit")} onDelete={action("onDelete")} />))
+    .add("Confirm", () => (<Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />))
