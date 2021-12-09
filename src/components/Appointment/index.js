73ad13c36118f9f9c0db62bd34f63c53a628b9f6
Jamie <lijamie19828@gmail.com>
2021-12-08 17:10:49 -0800

    create and fill scss, basic set up of index.js

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "components/Appointment/styles.scss"
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment"></article>
+  );
+}
