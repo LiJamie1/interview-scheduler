bba978d812ec44cd7519d7122d540f6bd3157870
Jamie <lijamie19828@gmail.com>
2021-12-09 20:39:58 -0800

    complete Promise.all

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,67 +4,39 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "components/Application.scss";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {}
-  })
-
-  const setDay = day => setState({...state, day });
-  const setDays = days => setState((prev) => ({...prev, days}));
-
-  const appointmentArray = appointments.map((appointment) => 
-    <Appointment
-    key={appointment.id}
-    interview={appointment.interview}
-    time={appointment.time}
-  />)
-
+  });
+  
+  const setDay = day => setState({ ...state, day });
+  
   useEffect(() => {
-    axios.get(`http://localhost:8001/api/days`)
-      .then(res => setDays(res.data))
-  }, [])
+    const urlBase = 'http://localhost:8001/api/'
+    const promiseDays = axios.get(`${urlBase}days`)
+    const promiseAppointments = axios.get(`${urlBase}appointments`)
+    
+    Promise.all([promiseDays, promiseAppointments])
+    .then((all) => {
+      const days = all[0].data;
+      const appointments = all[1].data;
+      console.log(days)
+      console.log(appointments)
+      setState((prev) => ({...prev, days, appointments}));
+    })
+  }, []);
+  
+  const dailyAppointments = [];
+
+  const appointmentArray = dailyAppointments.map(appointment =>
+    <Appointment
+      key={appointment.id}
+      interview={appointment.interview}
+      time={appointment.time}
+    />)
+
 
   return (
     <main className="layout">
