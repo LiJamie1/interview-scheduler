4ea9da3fa4ad2aba84e3eb18b60c1f0f535f972f
Jamie <lijamie19828@gmail.com>
2021-12-09 19:42:24 -0800

    add state to replace day and days, setState in progress

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,6 +47,11 @@ const appointments = [
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
   const [days, setDays] = useState([])
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
 
   const appointmentArray = appointments.map((appointment) => 
     <Appointment
@@ -73,8 +78,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
