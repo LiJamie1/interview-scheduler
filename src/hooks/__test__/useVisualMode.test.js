ddda99c91c4eef9a5620ba495e71e638a2ad25ed
Jamie <lijamie19828@gmail.com>
2021-12-10 17:18:22 -0800

    Create and test replace in transition

--- src/hooks/__test__/useVisualMode.test.js
+++ src/hooks/__test__/useVisualMode.test.js
@@ -35,3 +35,23 @@ test("useVisualMode should return to previous mode", () => {
   expect(result.current.mode).toBe(FIRST);
 });
 
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should replace the current mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+  act(() => result.current.transition(THIRD, true));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,12 +2,24 @@ import {useState} from "react"
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial])
 
-  const transition = (newMode) => {
+  function transition(newMode, replace = false) {
     setMode(newMode)
+    if (replace) {
+      setHistory((prev) => [...prev.slice(0,1), newMode])
+    } else {
+      setHistory((prev) => [...prev, newMode])
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      setMode(history[history.length-2])
+      setHistory(history.slice(0, -1))
+    }
   }
 
 
-
-  return {mode, transition}
+  return {mode, history, transition, back}
 }
