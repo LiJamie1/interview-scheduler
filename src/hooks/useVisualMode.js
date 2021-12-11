2f6f1df38b9b9e8571c0d7309a5fd3ea313cc1ba
Jamie <lijamie19828@gmail.com>
2021-12-10 16:29:20 -0800

    install react-test-renderer, create/test transition

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,13 @@
+import {useState} from "react"
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial)
+
+  const transition = (newMode) => {
+    setMode(newMode)
+  }
+
+
+
+  return {mode, transition}
+}
