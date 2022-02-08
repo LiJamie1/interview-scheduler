02c0598bbe4d02819eb05fd4c42a24619613a5f9
Jamie <lijamie19828@gmail.com>
2022-02-08 15:01:02 -0800

    complete and export reducer function

--- src/reducers/useApplicationDataReducer.js
+++ src/reducers/useApplicationDataReducer.js
@@ -1,16 +1,17 @@
-import updateInterviewSpots from "helpers/selectors"
+import updateInterviewSpots from "helpers/selectors";
 
 const SET_DAY = "SET_DAY";
 const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 const SET_INTERVIEW = "SET_INTERVIEW";
 
-function reducer(state, action) {
+export function reducer(state, action) {
   switch (action.type) {
     case SET_DAY:
       return {
         ...state,
         day: action.day,
       };
+
     case SET_APPLICATION_DATA:
       const { days, appointments, interviewers } = action.data;
       return {
@@ -19,13 +20,18 @@ function reducer(state, action) {
         appointments,
         interviewers,
       };
-    case SET_INTERVIEW:
-      const {id, interview} = action;
-      const appointment = {...state.appointments[id], interview}
-      const list_of_appointments = {...state.appointments, [id]: appointment}
 
-      const days = 
-      return {};
+    case SET_INTERVIEW:
+      const { id, interview } = action;
+      const appointment = { ...state.appointments[id], interview };
+      const list_of_appointments = { ...state.appointments, [id]: appointment };
+
+      const newDaysState = updateInterviewSpots(state, list_of_appointments);
+      return {
+        ...state,
+        list_of_appointments,
+        newDaysState,
+      };
     default:
       throw new Error(
         `Tried to reduce with unsupported action type: ${action.type}`
